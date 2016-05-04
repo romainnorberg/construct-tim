5d1ad6b97842f7bd3f40feba555ed8bb14485096
Romain Norberg <romainnorberg@gmail.com>
2016-05-04 19:44:44 +0000

    rollbar error test

---
+++ src/AppBundle/Controller/ErrorController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
+    Sensio\Bundle\FrameworkExtraBundle\Configuration\Method,
+    Symfony\Bundle\FrameworkBundle\Controller\Controller,
+    Symfony\Component\HttpFoundation\Response,
+    Symfony\Component\HttpFoundation\Request;
+
+class ErrorController extends Controller
+{
+
+  /**
+   * @Route("/error", name="error")
+   * @Method({"GET"})
+   */
+  public function errorAction(Request $request)
+  {
+    throw new \Exception('Something went wrong!');
+  }
+}
