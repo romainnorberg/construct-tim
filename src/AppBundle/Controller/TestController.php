fa8e7d72f1d64a9c808cf7e2f53e59ac23bc5d15
Romain Norberg <romainnorberg@gmail.com>
2016-05-16 15:52:41 +0000

    lipp filter tests

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -27,20 +27,6 @@ class TestController extends Controller
         );
     }
 
-    $filesystem = $this->get('knp_gaufrette.filesystem_map')->get('filesystem_aws_s3_images');
-        $file = $filesystem->get($projectType->getImageName());
-
-        /*
-         * Note the use of the dump() function.
-         * If you don't have the VarDumperComponent installed, use var_dump().
-         * @see http://symfony.com/doc/current/components/var_dumper/introduction.html
-         */
-        //dump($file);die;
-
-    //$helper = $this->container->get('vich_uploader.templating.helper.uploader_helper');
-    //$imagepath = $helper->asset($projectType, 'imageFile');
-    //
-
     $image = $projectType->getImageName();
 
     return $this->render(
