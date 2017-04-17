e7cc7d209549ea3c0d0d4d389eb33bfc5c9b3f2d
Romain Norberg <romainnorberg@gmail.com>
2017-04-17 09:49:24 +0200

    thanks travis

--- src/AppBundle/Entity/ProjectType.php
+++ src/AppBundle/Entity/ProjectType.php
@@ -265,6 +265,35 @@ class ProjectType
   }
 
   /**
+   * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+   * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+   * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+   * must be able to accept an instance of 'File' as the bundle will inject one here
+   * during Doctrine hydration.
+   *
+   * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+   *
+   * @return ProjectType
+   */
+  public function setImageFile(File $image = null)
+  {
+    $this->imageFile = $image;
+    if ($image) {
+      // It is required that at least one field changes if you are using doctrine
+      // otherwise the event listeners won't be called and the file is lost
+      $this->updated = new \DateTime('now');
+    }
+    return $this;
+  }
+  /**
+   * @return File
+   */
+  public function getImageFile()
+  {
+    return $this->imageFile;
+  }
+
+  /**
    * Set imageName
    *
    * @param string $imageName
